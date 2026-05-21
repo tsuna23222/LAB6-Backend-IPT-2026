588ff3775812b4eef11adcf0db3243fd1e387984
CASINILLO, Richard A, Jr. <111958140+tsuna23222@users.noreply.github.com>
2026-05-21 20:22:47 +0800

    Update email messages to use FRONTEND_URL

--- node-mysql-api/accounts/account.service.ts
+++ node-mysql-api/accounts/account.service.ts
@@ -224,15 +224,9 @@ function basicDetails(account: any) {
 }
 
 async function sendVerificationEmail(account: any, origin: any) {
-    let message;
-    if (origin) {
-        const verifyUrl = `${origin}/account/verify-email?token=${account.verificationToken}`;
-        message = `<p>Please click the below link to verify your email address:</p>
-                   <p><a href="${verifyUrl}">${verifyUrl}</a></p>`;
-    } else {
-        message = `<p>Please use the below token to verify your email address with the <code>/account/verify-email</code> api route:</p>
-                   <p><code>${account.verificationToken}</code></p>`;
-    }
+    const verifyUrl = `${process.env.FRONTEND_URL}/account/verify-email?token=${account.verificationToken}`;
+    const message = `<p>Please click the below link to verify your email address:</p>
+                     <p><a href="${verifyUrl}">${verifyUrl}</a></p>`;
 
     await sendEmail({
         to: account.email,
@@ -244,12 +238,7 @@ async function sendVerificationEmail(account: any, origin: any) {
 }
 
 async function sendAlreadyRegisteredEmail(email: any, origin: any) {
-    let message;
-    if (origin) {
-        message = `<p>If you don't know your password please visit the <a href="${origin}/account/forgot-password">forgot password</a> page.</p>`;
-    } else {
-        message = `<p>If you don't know your password you can reset it via the <code>/account/forgot-password</code> api route.</p>`;
-    }
+    const message = `<p>If you don't know your password please visit the <a href="${process.env.FRONTEND_URL}/account/forgot-password">forgot password</a> page.</p>`;
 
     await sendEmail({
         to: email,
@@ -261,15 +250,9 @@ async function sendAlreadyRegisteredEmail(email: any, origin: any) {
 }
 
 async function sendPasswordResetEmail(account: any, origin: any) {
-    let message;
-    if (origin) {
-        const resetUrl = `${origin}/account/reset-password?token=${account.resetToken}`;
-        message = `<p>Please click the below link to reset your password, the link will be valid for 1 day:</p>
-                   <p><a href="${resetUrl}">${resetUrl}</a></p>`;
-    } else {
-        message = `<p>Please use the below token to reset your password with the <code>/account/reset-password</code> api route:</p>
-                   <p><code>${account.resetToken}</code></p>`;
-    }
+    const resetUrl = `${process.env.FRONTEND_URL}/account/reset-password?token=${account.resetToken}`;
+    const message = `<p>Please click the below link to reset your password, the link will be valid for 1 day:</p>
+                     <p><a href="${resetUrl}">${resetUrl}</a></p>`;
 
     await sendEmail({
         to: account.email,
