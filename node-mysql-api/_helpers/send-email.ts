25395e3c3a7c3ea0f5fdb3e2cec7290df9b15995
CASINILLO, Richard A, Jr. <111958140+tsuna23222@users.noreply.github.com>
2026-05-21 19:56:33 +0800

    Improve email sending error handling
    Enhance error handling to log SendGrid error response.

--- node-mysql-api/_helpers/send-email.ts
+++ node-mysql-api/_helpers/send-email.ts
@@ -5,7 +5,10 @@ sgMail.setApiKey(process.env.SENDGRID_API_KEY as string);
 export default async function sendEmail({ to, subject, html, from = process.env.EMAIL_FROM }: any) {
     try {
         await sgMail.send({ to, from, subject, html });
-    } catch (error) {
-        console.error('Email sending failed:', error);
+    } catch (error: any) {
+    console.error('Email sending failed:', error);
+    if (error.response) {
+        console.error('SendGrid error body:', JSON.stringify(error.response.body));
     }
+}
 }
