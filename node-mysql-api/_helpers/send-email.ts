23d241729c4e99cb22f26e450998bd2179d34411
CASINILLO, Richard A, Jr. <111958140+tsuna23222@users.noreply.github.com>
2026-05-21 20:00:02 +0800

    Update send-email.ts

--- node-mysql-api/_helpers/send-email.ts
+++ node-mysql-api/_helpers/send-email.ts
@@ -2,13 +2,18 @@ import sgMail from '@sendgrid/mail';
 
 sgMail.setApiKey(process.env.SENDGRID_API_KEY as string);
 
-export default async function sendEmail({ to, subject, html, from = process.env.EMAIL_FROM }: any) {
+export default async function sendEmail({ to, subject, html }: any) {
     try {
-        await sgMail.send({ to, from, subject, html });
+        await sgMail.send({ 
+            to, 
+            from: 'richardcasinillohp@gmail.com', // hardcoded to test
+            subject, 
+            html 
+        });
     } catch (error: any) {
-    console.error('Email sending failed:', error);
-    if (error.response) {
-        console.error('SendGrid error body:', JSON.stringify(error.response.body));
+        console.error('Email sending failed:', error);
+        if (error.response) {
+            console.error('SendGrid error body:', JSON.stringify(error.response.body));
+        }
     }
-}
 }
